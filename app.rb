f17037df4a4cacbb19f4cfca8b7603231de29ee8
Randel Jiao <randeljiao@gmail.com>
2019-10-22 04:02:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,11 +5,11 @@ class App < Sinatra::Base
   get '/' do
     "<h1>Hello World</h1>"
 end
-    get '/index' do
+    get '/' do
       erb :index
     end
  
-    get "/info" do
+    get "/" do
       erb :info
     end
     
