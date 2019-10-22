afff6f7cd76216b5bc42ee8077864c31046deb7d
Randel Jiao <randeljiao@gmail.com>
2019-10-22 04:04:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
  
-  get '/' do
-    "<h1>Hello World</h1>"
-end
-    get '/index' do
+
+    get '/' do
       erb :index
     end
  
